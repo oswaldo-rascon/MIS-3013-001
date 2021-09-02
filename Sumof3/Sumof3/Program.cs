73dda180a5b15e96c682718567ff8ef76f627019
Oswaldo Rascon <Oswaldo.A.Rascon-1@ou.edu>
2021-09-02 11:28:06 -0500

    Sum of three numbers added

---
+++ Sumof3/Sumof3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sumof3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = 1;
+            double secondNumber = 2;
+            double thirdNumber = 3.2;
+
+            Console.WriteLine("Please enter first number. >>");
+            firstNumber = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter second number.>>");
+             secondNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter third number.>>" );
+             thirdNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Answer is {0}", firstNumber + secondNumber + thirdNumber);
+            Console.ReadKey();
+
+        }
+    }
+}
